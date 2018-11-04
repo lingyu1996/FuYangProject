951f169c07f35ab6dc8d510356fae1f267cef058
qin wenqiang <qinwenqiang@lenovo.com>
2018-11-04 14:52:11 +0800

    commit by qinwenqiang

--- app/src/main/java/com/pingan/fuyanghospital/activity/MainActivity.java
+++ app/src/main/java/com/pingan/fuyanghospital/activity/MainActivity.java
@@ -1,7 +1,10 @@
-package com.pingan.fuyanghospital;
+package com.pingan.fuyanghospital.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.pingan.fuyanghospital.R;
+import com.pingan.fuyanghospital.constances.Constants;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/pingan/fuyanghospital/constances/Constants.java
@@ -0,0 +1,5 @@
+package com.pingan.fuyanghospital.constances;
+
+public interface Constants {
+    String PACKAGE_NAME = "com.pingan.fuyanghospital";
+}
