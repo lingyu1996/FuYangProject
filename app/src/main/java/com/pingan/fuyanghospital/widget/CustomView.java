9c7531038acfd346b0ff21eb6e948217cbcc354a
qin wenqiang <qinwenqiang@lenovo.com>
2018-11-04 15:19:57 +0800

    commit by qinwenqiang

---
+++ app/src/main/java/com/pingan/fuyanghospital/widget/CustomView.java
@@ -0,0 +1,21 @@
+package com.pingan.fuyanghospital.widget;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+
+public class CustomView extends View {
+    public CustomView(Context context) {
+        super(context);
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
