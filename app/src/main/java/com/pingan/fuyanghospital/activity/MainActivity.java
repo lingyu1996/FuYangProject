2a3ef2ad221b0479b5ba4da7836eab4c68aad7dd
qin wenqiang <qinwenqiang@lenovo.com>
2018-11-05 09:51:37 +0800

    commit by qinwenqiang

--- app/src/main/java/com/pingan/fuyanghospital/activity/MainActivity.java
+++ app/src/main/java/com/pingan/fuyanghospital/activity/MainActivity.java
@@ -1,10 +1,9 @@
 package com.pingan.fuyanghospital.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.pingan.fuyanghospital.R;
-import com.pingan.fuyanghospital.constances.Constants;
 
 public class MainActivity extends AppCompatActivity {
 
